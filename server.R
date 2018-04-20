9d5001601a39cff2ae8b38507349e68e7ca2d53b
XiaoWu <34700197+EchoWOO@users.noreply.github.com>
2018-04-20 10:46:27 -0700

    Uploaded images

--- server.R
+++ server.R
@@ -6,21 +6,22 @@ library(leaflet)
 
 shinyServer(function(input, output, session) {
   
+  drv <- dbDriver("PostgreSQL")
+  
+  con <- dbConnect(drv, dbname = "Assign2",
+                   host = "127.0.0.1", port = 5432,
+                   user = "postgres", password = 'xx6161')
+  
   tracts = st_read_db(con, query = "SELECT * FROM tract16withld", geom_column = 'wkb_geometry')
   
-  # points = st_read_db(con, query = "SELECT * FROM tract16ct", geom_column = 'wkb_geometry')
-  
-  #bigtracts <- filter(world.cities, pop > 1000000)
-  
-  # tracts$pct_ebll_county <- gsub("%","", tracts$pct_ebll_county)
-  # 
-  # tracts$pct_ebll_county <- as.numeric(tracts$pct_ebll_county)
-  
-  # points$pct_ebll_county <- gsub("%","", points$pct_ebll_county)
-  # 
-  # points$pct_ebll_county <- as.numeric(points$pct_ebll_county)
+  pal_blue11 = c("#d8f2ed",
+                 "#bfded8", "#a9ccc6", "#93bab4", "#7ea8a2",
+                 "#6b9993", "#588782", "#477872", "#376b66", 
+                 "#265c56", "#154f4a")
   
   
+  paletteContinuousC <- colorNumeric(palette = pal_blue11, domain = tracts$per_eblls_label)
+
   output$selectEBLL <- renderUI({
     EBLLlist <- arrange(tracts, per_eblls_label)
     EBLLlist <- unique(EBLLlist$per_eblls_label)
@@ -48,20 +49,40 @@ shinyServer(function(input, output, session) {
     
   })
   
-  
-  
   output$mymap <- renderLeaflet({
     
     leaflet() %>%
       addProviderTiles(providers$CartoDB.PositronNoLabels) %>%
       addPolygons( data = selecttracts(),
                    # important () to use the interactive data,
-                   fillColor = "blue",
+                   fillColor = ~paletteContinuousC(per_eblls_label),
                    weight = 0.8,
                    opacity = 0.6,
                    smoothFactor = 0.1,
                    color = "white",
-                   fillOpacity = 0.8)
+                   fillOpacity = 0.8,
+                   highlight = highlightOptions(
+                     fillColor = "yellow",
+                     fillOpacity = 0.8,
+                     weight = 2,
+                     bringToFront = TRUE),
+                    popup = paste0("<strong>TractID: </strong>",
+                                   selecttracts()$id,
+                                   "<br/><strong>Percent of children with EBLL: </strong>",
+                                   selecttracts()$per_eblls_label,
+                                   "<br/><strong>Children tested: </strong>",
+                                   selecttracts()$tested,
+                                   "<br/><strong>Percent of children with EBLL in county: </strong>",
+                                   selecttracts()$pct_ebll_county,
+                                   "<br/><strong>Percent EBLLs compared to Minnesota: </strong>",
+                                   selecttracts()$pct_ebll_cat_label,
+                                   "<br/><strong>Total Population in Tract: </strong>",
+                                   selecttracts()$TotalPopulation,
+                                   "<br/><strong>Total Number of Households with Children: </strong>",
+                                   selecttracts()$Totalchild,
+                                   "<br/><strong>Median House Income: </strong>",
+                                   selecttracts()$MEDINC12)
+                   )
   })
   
   
